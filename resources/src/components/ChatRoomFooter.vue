5f95ddc8b7f9da5faf9d45dec329618ba9c2ff6e
agik-setiawan <setiawanagik@gmail.com>
2020-09-01 17:40:33 +0700

    enter message

--- resources/src/components/ChatRoomFooter.vue
+++ resources/src/components/ChatRoomFooter.vue
@@ -9,7 +9,13 @@
       </div>
       <div class="footer-content" v-if="group_active">
         <div class="chatbox-container">
-          <input type="text" class="chatbox-text" placeholder="Type Message" v-model="message" />
+          <input
+            type="text"
+            class="chatbox-text"
+            placeholder="Type Message"
+            v-model="message"
+            id="chat-box-input"
+          />
         </div>
         <div class="tools">
           <div>
@@ -94,9 +100,22 @@ export default {
         group_id: this.group_selected.group_id,
         message: this.message,
       });
-      this.message="";
+      this.message = "";
     },
   },
-  mounted() {},
+  mounted() {
+    let _this = this;
+    var input = document.getElementById("chat-box-inpu");
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function (event) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        _this.sendMessage();
+      }
+    });
+  },
 };
 </script>
